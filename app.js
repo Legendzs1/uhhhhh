6fe583207d5d98745b6b2367d315d34f20e00492
Legendzs1 <46654819+Legendzs1@users.noreply.github.com>
2023-04-03 17:59:52 -0400

    created basic redis connection that consoles a value from redis

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-let helloWorld = 'Hello World'
-console.log(helloWorld)
+import clientConnection from './src/client.js'
+
+
+let client = new clientConnection();
+
+client.set('key','value');
+
+const value = await client.get('key');
+
+console.log(value)
